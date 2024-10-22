c3024b455e2249a7df2a016e479b3aeb944d0497
Xingda Wang <xingdawang@MACBOOK-PRO.station>
2024-10-22 16:23:47 +0100

    comment off register function

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
+    # path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('edit_profile/', views.edit_profile, name='edit_profile'),
